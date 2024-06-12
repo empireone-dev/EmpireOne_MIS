c70eb279962ce1f44dc9d8e6aab6408a890da653
EOGSqwerty <scitdept@empireonegroup.com>
2024-06-12 17:48:15 +0800

    Created pre-employment UI page

---
+++ resources/js/app/pages/pre-employment/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PreEmploymentDetailsSection from './sections/pre-employment-details-section'
+import PreEmploymentTableSection from './sections/pre-employment-table-section'
+
+export default function PreEmploymentPage() {
+  return (
+    <div>
+        {/* <img src="/images/scemployee.jpg" className="absolute inset-0 w-full h-full object-cover"/> */}
+        <PreEmploymentDetailsSection/>
+    </div>
+  )
+}

62eaeda0a2f7c704fc49ee60a595ddab814f6451
QuicklyDG <quicklydeguzman@gmail.com>
2024-08-04 15:40:46 +0800

    created engagement announce modal

--- resources/js/app/pages/admin/engagement_section/engagement_dashboard/sections/add-engagement-announcement-section.jsx
+++ resources/js/app/pages/admin/engagement_section/engagement_dashboard/sections/add-engagement-announcement-section.jsx
@@ -1,18 +1,117 @@
-import { MegaphoneIcon, PencilSquareIcon } from '@heroicons/react/24/outline'
-import React from 'react'
+import { EditFilled } from '@ant-design/icons';
+import { CameraIcon, DocumentTextIcon, MegaphoneIcon, PhotoIcon } from '@heroicons/react/24/outline';
+import { Button, Modal } from 'antd';
+import React, { useState, useEffect } from 'react';
 
 export default function AddEngagementAnnouncementSection() {
+    const [announceTitle, setAnnounceTitle] = useState('');
+    const [site, setSite] = useState('');
+    const [announceDesc, setAnnounceDesc] = useState('');
+    const [open, setOpen] = useState(false);
+    const [date, setDate] = useState('');
+
+    useEffect(() => {
+        const today = new Date().toISOString().split('T')[0];
+        setDate(today);
+    }, []);
+
     return (
         <div className="my-2">
-            <div class="inline-flex rounded-md shadow-sm" role="group">
+            <div className="inline-flex rounded-md shadow-sm" role="group">
                 <button
                     type="button"
-                    class="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-500 bg-transparent border border-blue-500 rounded-lg hover:bg-blue-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-500 focus:text-white      gap-1"
+                    onClick={() => setOpen(true)}
+                    className="inline-flex items-center px-4 py-2 text-sm font-medium text-blue-500 bg-transparent border border-blue-500 rounded-lg hover:bg-blue-500 hover:text-white focus:z-10 focus:ring-2 focus:ring-blue-500 focus:bg-blue-500 focus:text-white gap-1"
                 >
-                    <MegaphoneIcon className='h-4'  />
+                    <MegaphoneIcon className='h-4' />
                     Create Announcement
                 </button>
             </div>
+            <Modal
+                title="Create Announcement"
+                centered
+                open={open}
+                onOk={() => setOpen(false)}
+                onCancel={() => setOpen(false)}
+                width={1000}
+                footer={null}
+            >
+                <form className="w-full">
+                    <div className="flex flex-wrap -mx-3 mb-6">
+                        <div className='flex flex-1'>
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-text">
+                                    Announcement Title:
+                                </label>
+                                <input className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" value={announceTitle} onChange={(e) => setAnnounceTitle(e.target.value)} id="grid-text" type="text" placeholder="" />
+                            </div>
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-text">
+                                    Site:
+                                </label>
+                                <select
+                                    value={site}
+                                    onChange={(e) => setSite(e.target.value)}
+                                    className='appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500'
+                                    name="site"
+                                    id="site">
+                                    <option></option>
+                                    <option>San Carlos</option>
+                                    <option>Carcar</option>
+                                    <option>Both</option>
+                                </select>
+                            </div>
+                            <div className="w-full px-3">
+                                <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-text">
+                                    Date:
+                                </label>
+                                <input
+                                    className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                    id="grid-text"
+                                    type="date"
+                                    value={date}
+                                    onChange={(e) => setDate(e.target.value)}
+                                    readOnly
+                                />
+                            </div>
+                        </div>
+                        <div className="w-full px-3">
+                            <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-text">
+                                Announcement Description:
+                            </label>
+                            <textarea
+                                className="appearance-none block w-full h-32 border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                value={announceDesc}
+                                onChange={(e) => setAnnounceDesc(e.target.value)}
+                                id="grid-text"
+                                placeholder=""
+                            />
+                        </div>
+                        <div className="flex flex-wrap items-center mt-1 px-3">
+                            <label htmlFor="image-upload" className="flex items-center mr-4 cursor-pointer text-gray-600 hover:text-gray-900">
+                                <PhotoIcon className='h-6' />
+                                <span>Upload Image</span>
+                                <input id="image-upload" name="image-upload" type="file" className="sr-only" />
+                            </label>
+                            <label htmlFor="video-upload" className="flex mr-4  items-center cursor-pointer text-gray-600 hover:text-gray-900">
+                                <CameraIcon className='h-6' />
+                                <span>Upload Video</span>
+                                <input id="video-upload" name="video-upload" type="file" className="sr-only" />
+                            </label>
+                            <label htmlFor="image-upload" className="flex items-center mr-4 cursor-pointer text-gray-600 hover:text-gray-900">
+                                <DocumentTextIcon className='h-6' />
+                                <span>Upload PDF File</span>
+                                <input id="image-upload" name="image-upload" type="file" className="sr-only" />
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex justify-end mt-3.5">
+                        <button className={`rounded-md w-full bg-blue-600 px-4 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 ${!(announceTitle || site || announceDesc) && 'cursor-not-allowed opacity-50'}`} disabled={!announceTitle && !site && !announceDesc}>
+                            <EditFilled /> POST
+                        </button>
+                    </div>
+                </form>
+            </Modal>
         </div>
-    )
+    );
 }
--- resources/js/app/pages/login/page.jsx
+++ resources/js/app/pages/login/page.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
-import LoginFormSection from './sections/login-form-section';
 import videoBackground from './../../../../../public/images/design.mp4';
+import { useForm, usePage } from '@inertiajs/react';
+import { useEffect } from 'react';
 
-export default function Page() {
+export default function Page({ status, canResetPassword }) {
+  const { url } = usePage();
+  const searchStatus = url.split("=")[1];
+  const { data, setData, post, processing, errors, reset } = useForm({
+    email: "",
+    password: "",
+    remember: false,
+  });
+
+  useEffect(() => {
+    return () => {
+      reset("password");
+    };
+  }, []);
+  function submitLogin(e) {
+    e.preventDefault();
+     post(route("login"));
+  }
+
+  function formHandler(value, email) {
+    setData(email, value);
+  }
+
   return (
     <div className="relative w-screen h-screen">
       <video autoPlay loop muted className="absolute inset-0 w-full h-full object-cover">
@@ -11,7 +34,113 @@ export default function Page() {
       </video>
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="relative z-10">
-          <LoginFormSection />
+          <form
+            onSubmit={submitLogin}
+          >
+            <section>
+              <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="w-full max-w-md bg-white rounded-2xl shadow-2xl">
+                  <a href="#" className="flex items-center justify-center p-3">
+                    <img className="w-full" src="images/newlogo.png" alt="logo" />
+                  </a>
+                  <div className="px-8 pt-6 pb-8 md:px-10">
+                    <h1 className="text-2xl font-bold leading-tight tracking-tight mb-6 text-center md:text-3xl">
+                      Sign in to your account
+                    </h1>
+                    <form className="space-y-4" action="#">
+                      <div class="flex flex-col gap-6 w-full">
+                        <div className="w-full">
+                          <div className="relative">
+                            <input
+                              required
+                              value={data?.email ?? ""}
+                              onChange={(e) =>
+                                setData({
+                                  ...data,
+                                  [e.target.name]:e.target.value
+                                })
+                              }
+                              type={'text'}
+                              id="email"
+                              name='email'
+                              className="peer text-black rounded-md placeholder-transparent w-full py-2.5 px-5 border-gray-500 border bg-transparent  bg-white focus-within:outline-none focus-within:border-blue-500"
+                              placeholder=""
+                            />
+                            <label
+                              htmlFor='email'
+                              className="absolute left-2.5 px-2.5 transition-all bg-white text-blue-black/60 text-sm -top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2.5 peer-focus:-top-3 peer-focus:text-sm peer-focus:text-blue-600 peer-focus:bg-white"
+                            >
+                              Employee ID
+                            </label>
+                          </div>
+                          {errors.email ==
+                            "These credentials do not match our records." &&
+                            (data?.email ?? "") !== "" && (
+                              <p className="text-red-500 text-sm mt-1.5 font-light">
+                                {errors.email}
+                              </p>
+                            )}
+
+                        </div>
+                        <div className="w-full">
+                          <div className="relative">
+                            <input
+                              required={true}
+                              value={data?.password ?? ""}
+                              onChange={(e) =>
+                                setData({
+                                  ...data,
+                                  [e.target.name]:e.target.value
+                                })
+                              }
+                              type={'password'}
+                              id="password"
+                              name='password'
+                              className="peer text-black placeholder-transparent w-full py-2.5 px-5 border-gray-500 border bg-transparent rounded-md bg-white focus-within:outline-none focus-within:border-blue-500"
+                              placeholder="Hello"
+                            />
+                            <label
+                              htmlFor='password'
+                              className="absolute left-2.5 px-2.5 transition-all bg-white text-blue-black/60 text-sm -top-3 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2.5 peer-focus:-top-3 peer-focus:text-sm peer-focus:text-blue-600 peer-focus:bg-white"
+                            >
+                              Password
+                            </label>
+                          </div>
+                          {errors.password ==
+                            "These credentials do not match our records." &&
+                            (data?.password ?? "") !== "" && (
+                              <p className="text-red-500 text-sm mt-1.5 font-light">
+                                {errors.password}
+                              </p>
+                            )}
+
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <div className="flex items-start">
+                          <input
+                            id="remember"
+                            aria-describedby="remember"
+                            type="checkbox"
+                            className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300"
+                          />
+                          <label htmlFor="remember" className="ml-2 text-sm">Remember me</label>
+                        </div>
+                        <a href="#" className="text-sm font-medium text-blue-600 hover:underline">Forgot password?</a>
+                      </div>
+                      <button
+                      onClick={submitLogin}
+                        type="submit"
+                        className="w-full mt-4 text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm py-2"
+                      >
+                        Sign in
+                      </button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </form>
         </div>
       </div>
     </div>

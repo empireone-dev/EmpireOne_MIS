285ee71526118b644238e9e33c47c646d463bbe5
QuicklyDG <quicklydeguzman@gmail.com>
2024-08-20 02:57:49 +0800

    fixed some bugs

--- resources/js/app/pages/admin/recruitment/guide_question/section/edit-guide-question-section.jsx
+++ resources/js/app/pages/admin/recruitment/guide_question/section/edit-guide-question-section.jsx
@@ -17,7 +17,7 @@ export default function EditGuideQuestionSection({ data, label, icon }) {
         e.preventDefault()
         try {
             store.dispatch(update_guide_question_thunk(dataValue))
-            messageApi.success('success');
+            messageApi.success('Updated Successfully');
             setShowModal(false)
         } catch (error) {
             messageApi.error('error');
@@ -25,51 +25,59 @@ export default function EditGuideQuestionSection({ data, label, icon }) {
     }
     return (
         <div>
-
             {contextHolder}
             <Menu.Item key={data.key} icon={icon}>
                 <button onClick={() => setShowModal(true)}>{label}</button>
                 <Modal
                     title={label}
                     centered
-                    visible={showModal}
+                    open={showModal}
                     footer={null}
                     onOk={() => setShowModal(false)}
                     onCancel={() => setShowModal(false)}
-                    width={520} // Adjusted width for better UI
-                    okText="Save Changes"
-                    cancelText="Cancel"
+                    width={520}
                 >
-                    <form className="w-full">
-                        <div className="flex flex-wrap -mx-3 mb-6">
+                    <div className="w-full">
+                        <div className="flex flex-wrap -mx-3">
                             <div className="w-full px-3">
                                 <form
                                     onSubmit={submit_edit}
                                     className="w-full">
-                                    <div className="flex flex-wrap -mx-3 mb-6">
+                                    <div className="flex flex-wrap -mx-3 mt-2">
                                         <div className="w-full px-3">
-                                            <label className="block uppercase tracking-wide text-xs font-bold mb-2" htmlFor="grid-text">
+                                            <label className="block uppercase tracking-wide text-xs font-bold mb-2">
                                                 Guide Question
                                             </label>
                                             <input
-                                                value={dataValue?.guideqs}
+                                                defaultValue={dataValue?.guideqs}
                                                 onChange={(e) => setDataValue({
                                                     ...dataValue,
                                                     guideqs: e.target.value
                                                 })}
-                                                className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-text" type="text" placeholder="" />
+                                                name='guideqs'
+                                                className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                                                 type="text" 
+                                                 placeholder="" />
                                         </div>
                                     </div>
-                                    <button
-                                        onClick={submit_edit}
-                                        type='submit'>
-                                        Edit
-                                    </button>
+                                    <div className='flex flex-1 gap-1.5 justify-end mt-1'>
+                                        <button
+                                            className='flex justify-end px-2 py-1.5 rounded-md hover:bg-gray-300'
+                                            onClick={() => setShowModal(false)}
+                                            type='button'>
+                                            Cancel
+                                        </button>
+                                        <button
+                                            className='flex justify-end bg-blue-600 px-2 py-1.5 rounded-md text-white hover:bg-blue-500'
+                                            onClick={submit_edit}
+                                            type='submit'>
+                                            Save Changes
+                                        </button>
+                                    </div>
                                 </form>
-                                {/* <input className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-text" type="text" placeholder="" /> */}
                             </div>
                         </div>
-                    </form>
+                    </div>
                 </Modal>
             </Menu.Item>
 

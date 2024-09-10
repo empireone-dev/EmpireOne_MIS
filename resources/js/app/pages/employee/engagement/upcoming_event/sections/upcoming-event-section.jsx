ca06a73c5bc76256ef13d1ce482af620bc53adba
QuicklyDG <quicklydeguzman@gmail.com>
2024-09-10 23:33:09 +0800

    commit changes

--- resources/js/app/pages/employee/engagement/upcoming_event/sections/upcoming-event-section.jsx
+++ resources/js/app/pages/employee/engagement/upcoming_event/sections/upcoming-event-section.jsx
@@ -1,16 +1,18 @@
 import React from "react";
 import UpcomingEventsCardComponent from "../components/upcoming-events-card-component";
+import { CalendarDaysIcon } from "@heroicons/react/24/outline";
 
 export default function UpcomingEventSection() {
     return (
         <div>
             <div className="rounded-lg p-3 flex flex-col">
                 <div className=" flex flex-1 bg-blue-400 p-6 rounded-t-lg mb-5">
-                    <h1 className="text-4xl font-sans text-white w-full">
+                    <h1 className="flex gap-1 text-4xl font-sans text-white w-full">
+                        <CalendarDaysIcon className="h-9"/>
                         <b>Upcoming Event(s):</b> 10
                     </h1>
-                    <div className="text-white text-lg w-full justify-end flex">
-                        <button>
+                    <div className="text-white text-lg w-full flex justify-center lg:justify-end">
+                        <button className="bg-white text-blue-500 px-4 py-2 rounded-lg hover:text-blue-600">
                             <b>View all Events</b>
                         </button>
                     </div>

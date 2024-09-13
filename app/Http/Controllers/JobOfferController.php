8a1ad5495c076ed4e90e104233ec5995ad55f22b
QuicklyDG <quicklydeguzman@gmail.com>
2024-09-13 15:15:59 -0400

    adjust job offer section

--- app/Http/Controllers/JobOfferController.php
+++ app/Http/Controllers/JobOfferController.php
@@ -10,15 +10,34 @@ use Illuminate\Support\Facades\Mail;
 
 class JobOfferController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         $joboffer = JobOffer::with('applicant')->get();
         return response()->json([
             'data' => $joboffer
         ], 200);
     }
-    public function store(Request $request){
+    public function store(Request $request)
+    {
         $data = $request->all();
         JobOffer::create($request->all());
         Mail::to($request->email)->send(new MailJobOffer($data));
+    }
+
+    public function update(Request $request, $id)
+    {
+        $jo = JobOffer::where('id', '=',  $request->id)->first();
+        $jo->update([
+            'status' => $request->status,
+            'reas' => $request->reas
+        ]);
+        if ($request->status == 'Accepted') {
+            Applicant::where('app_id', $id)->update([
+                'status' => 'Contract Signing'
+            ]);
+        }
+        return response()->json([
+            'data' => 'success'
+        ], 200);
     }
 }

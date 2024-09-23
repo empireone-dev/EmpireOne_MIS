91f774b4a32d01d03dd4ff828471e64f7623d567
QuicklyDG <quicklydeguzman@gmail.com>
2024-09-24 00:34:02 +0800

    create 201 file data params

--- resources/js/app/pages/admin/file_201/sections/file-201-table-section.jsx
+++ resources/js/app/pages/admin/file_201/sections/file-201-table-section.jsx
@@ -8,6 +8,7 @@ import File201ButtonSection from './file-201-button-section';
 import store from '@/app/store/store';
 import { get_checklist_thunk } from '../../hiring/pre_employment/redux/pre-employment-thunk';
 import { useEffect } from 'react';
+import File201ImageSection from './file-201-image-section';
 
 export default function File201TableSection() {
     const [searchText, setSearchText] = useState('');
@@ -149,13 +150,7 @@ export default function File201TableSection() {
             dataIndex: 'action',
             render: (_, record) => (
                 <div className='flex flex-1 gap-1'>
-                    <button
-                        type="button"
-                        onClick={() => setModalVisible(true)}
-                        className="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300  shadow-lg shadow-cyan-500/50 font-medium rounded-lg text-lg px-3.5 py-2 text-center"
-                    >
-                        <PictureFilled />
-                    </button>
+                <File201ImageSection data={record}/>
                     <button
                         type="button"
                         className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50 font-medium rounded-lg text-lg px-3.5 py-2 text-center"
@@ -173,28 +168,7 @@ export default function File201TableSection() {
     return (
         <div>
             {/* Modal component */}
-            <Modal
-                title="(Pre Employment Requirements' Name)"
-                visible={modalVisible}
-                onCancel={() => setModalVisible(false)}
-                footer={null}
-            >
-                <p>Sample Image</p>
-                <img src="/images/SCemp.jpg" alt="" />
-                <div className='flex flex-1 gap-1 w-full items-center justify-center mt-2 text-white'>
-                    <div className='flex w-full items-center justify-center'>
-                        <button className='bg-green-500 w-full rounded hover:bg-green-600 p-0.5'>
-                            Approved
-                        </button>
-                    </div>
-                    <div className='flex w-full items-center justify-center'>
-                        <button className='bg-red-500 w-full rounded hover:bg-red-600 p-0.5'>
-                            Declined
-                        </button>
-                    </div>
-                </div>
-            </Modal>
-
+      
             <div>
                 <div className="flex items-center gap-x-3 mb-4">
                     <h2 className="text-lg font-medium text-gray-800">

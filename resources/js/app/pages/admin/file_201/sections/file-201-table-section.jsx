a75cc091a284dab6e3196272e799d2d51abc4b8d
QuicklyDG <quicklydeguzman@gmail.com>
2024-09-27 07:18:38 -0400

    make new job offer on onboarding ack table

--- resources/js/app/pages/admin/file_201/sections/file-201-table-section.jsx
+++ resources/js/app/pages/admin/file_201/sections/file-201-table-section.jsx
@@ -16,7 +16,7 @@ export default function File201TableSection() {
     const [modalVisible, setModalVisible] = useState(false);
     const searchInput = useRef(null);
     const { applicant } = useSelector((state) => state.final_rate);
-    console.log('applicant',applicant.requirements)
+    // console.log('applicant',applicant.requirements)
 
     useEffect(() => {
         store.dispatch(get_checklist_thunk())
--- resources/js/app/pages/admin/onboarding/acknowledgement/page.jsx
+++ resources/js/app/pages/admin/onboarding/acknowledgement/page.jsx
@@ -4,10 +4,14 @@ import AcknowledgementTableSection from './sections/acknowledgement-table-sectio
 import store from '@/app/store/store';
 import { get_job_offer_thunk } from './redux/acknowledgement-thunk';
 import { useEffect } from 'react';
+import { get_applicant_thunk } from '../../recruitment/applicants/applicant_records/redux/applicant-thunk';
+import { get_job_position_thunk } from '../../sourcing/job_title_section/redux/job-title-thunk';
 
 export default function AcknowledgementPage() {
   useEffect(() => {
+    store.dispatch(get_applicant_thunk())
     store.dispatch(get_job_offer_thunk())
+    store.dispatch(get_job_position_thunk())
   }, []);
   return (
     <AdminLayout>

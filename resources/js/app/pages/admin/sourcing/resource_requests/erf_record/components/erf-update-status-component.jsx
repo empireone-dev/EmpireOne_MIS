b22d3ba296cb03e15caa63f108f7963b7360afc2
QuicklyDG <quicklydeguzman@gmail.com>
2024-10-14 00:51:27 +0800

    erf update

--- resources/js/app/pages/admin/sourcing/resource_requests/erf_record/components/erf-update-status-component.jsx
+++ resources/js/app/pages/admin/sourcing/resource_requests/erf_record/components/erf-update-status-component.jsx
@@ -1,63 +1,129 @@
-import { Menu, Modal } from 'antd';
-import React, { useState } from 'react'
+import { Menu, message, Modal } from 'antd';
+import React, { useEffect, useState } from 'react';
+import { get_erf_record_thunk, update_outsourcing_erf_thunk } from '../redux/erf-record-thunk';
+import store from '@/app/store/store';
 
 export default function ErfUpdateStatusComponent({ data, item }) {
-    const [modalOpen, setModalOpen] = useState(false);
-    function openHandler(params) {
-        setModalOpen(true);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [dataValue, setDataValue] = useState({});
+
+  useEffect(() => {
+    if (data) {
+      setDataValue(data);
     }
-    return (
-        <>
-            <Menu.Item onClick={() => openHandler(true)} icon={item.icon}>
-                {item.label}
-            </Menu.Item>
-           
+  }, [data]);
+
+  function openHandler() {
+    setModalOpen(true);
+  }
+
+  async function edit_outsourcing_erf(e) {
+    e.preventDefault();
+
+    if (!dataValue.status) {
+      message.error('Please select a valid status.');
+      return;
+    }
+
+    try {
+      await store.dispatch(update_outsourcing_erf_thunk(dataValue));
+      await store.dispatch(get_erf_record_thunk())
+      message.success('Updated Successfully');
+      setModalOpen(false);
+    } catch (error) {
+      message.error('Error updating status. Please try again.');
+      console.error(error);
+    }
+  }
+
+  return (
+    <>
+      <Menu.Item onClick={openHandler} icon={item.icon}>
+        {item.label}
+      </Menu.Item>
+
       <Modal
         title="Update Request Status"
         centered
         visible={modalOpen}
-        onOk={() => setModalOpen(false)}
         onCancel={() => setModalOpen(false)}
         width={1200}
-        okText="Save Changes"
-        cancelText="Cancel"
+        footer={null}
       >
-        <form className="w-full pb-4">
+        <form className="w-full pb-4" onSubmit={edit_outsourcing_erf}>
           <div className="flex flex-wrap -mx-3 mb-6">
             <div className='flex flex-1 '>
-              <div class="w-full px-3">
-                <label class="block uppercase tracking-wide  text-xs font-bold mb-1 mt-2" for="grid-text">
+              <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-xs font-bold mb-1 mt-2">
                   Reference No.
                 </label>
-                <input class="appearance-none block w-full   border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" value={data?.ref_id} readOnly />
+                <input
+                  className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="text"
+                  value={data?.ref_id || ''}
+                  readOnly
+                />
               </div>
-              <div class="w-full px-3">
-                <label class="block uppercase tracking-wide  text-xs font-bold mb-1 mt-2" for="grid-text">
+              <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-xs font-bold mb-1 mt-2">
                   Job Title
                 </label>
-                <input class="appearance-none block w-full   border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" value={data?.jobTitle} readOnly />
+                <input
+                  className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="text"
+                  value={data?.jobTitle || ''}
+                  readOnly
+                />
               </div>
-              <div class="w-full px-3">
-                <label class="block uppercase tracking-wide  text-xs font-bold mb-1 mt-2" for="grid-text">
+              <div className="w-full px-3">
+                <label className="block uppercase tracking-wide text-xs font-bold mb-1 mt-2">
                   Job Type
                 </label>
-                <input class="appearance-none block w-full   border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" value={data?.jobType} readOnly />
+                <input
+                  className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  type="text"
+                  value={data?.jobType || ''}
+                  readOnly
+                />
               </div>
             </div>
-            <div class="w-full px-3">
-              <label class="block uppercase tracking-wide  text-xs font-bold mb-1 mt-2" for="grid-text">
+            <div className="w-full px-3">
+              <label className="block uppercase tracking-wide text-xs font-bold mb-1 mt-2">
                 Request Status
               </label>
-              <select class="appearance-none block w-full   border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" name="" id="">
-                <option value=""></option>
-                <option value="">In Review</option>
-                <option value="">Approved</option>
-                <option value="">Declined</option>
+              <select
+                onChange={(e) => setDataValue({
+                  ...dataValue,
+                  status: e.target.value
+                })}
+                className="appearance-none block w-full border border-gray-400 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                defaultValue={data?.status || ''}
+              >
+                <option value="" disabled>Select status</option>
+                <option value="Pending" disabled>Pending</option>
+                <option value="In Review">In Review</option>
+                <option value="Approved">Approved</option>
+                <option value="Declined">Declined</option>
               </select>
+            </div>
+            <div className='flex flex-1 gap-1.5 justify-end mt-1'>
+              <button
+                className='flex justify-end px-2 py-1.5 rounded-md hover:bg-gray-300'
+                onClick={() => setModalOpen(false)}
+                type='button'
+              >
+                Cancel
+              </button>
+              <button
+                className='flex justify-end bg-blue-600 px-2 py-1.5 rounded-md text-white hover:bg-blue-500'
+                type='submit'
+              >
+                Save Changes
+              </button>
             </div>
           </div>
         </form>
       </Modal>
-        </>
-    )
+    </>
+  );
 }

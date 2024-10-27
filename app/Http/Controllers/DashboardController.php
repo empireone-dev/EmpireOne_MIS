3176ac0b5db9d28256e000d64dd078acc8905338
QuicklyDG <quicklydeguzman@gmail.com>
2024-10-28 04:16:30 +0800

    dashboard for sourcing and recruitment

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -15,17 +15,24 @@ class DashboardController extends Controller
         $ongoing = OutSourcingErf::where('status', '=', 'In Review')->count();
         $declined = OutSourcingErf::where('status', '=', 'Declined')->count();
 
-        $active = Applicant::whereIn('status', ['Approved', 'Active'])->count();
-        $pending = Applicant::where('status', '=', 'Pending')->count();
-        $final = Applicant::where('status', '=', 'Final Phase')->count();
-        $declined = Applicant::where('status', '=', 'Declined')->count();
-    
+        $app_active = Applicant::whereIn('status', ['Approved', 'Active'])->count();
+        $app_pending = Applicant::where('status', '=', 'Pending')->count();
+        $app_initial = Applicant::where('status', '=', 'Initial Phase')->count();
+        $app_final = Applicant::where('status', '=', 'Final Phase')->count();
+        $app_passed = Applicant::where('status', '=', 'Passed')->count();
+        $app_failed = Applicant::where('status', '=', 'Failed')->count();
+
         return response()->json([
             'active' => $active,
             'pending' => $pending,
             'ongoing' => $ongoing,
             'declined' => $declined,
-            'final' => $final
+            'app_initial' => $app_initial,
+            'app_final' => $app_final,
+            'app_active' => $app_active,
+            'app_pending' => $app_pending,
+            'app_failed' => $app_failed,
+            'app_passed' => $app_passed,
 
         ], 200);
     }

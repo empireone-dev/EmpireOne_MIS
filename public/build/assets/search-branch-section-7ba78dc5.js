faca97b0f448e8a28025caa01c7c1cd0c47334f6
QuicklyDG <quicklydeguzman@gmail.com>
2025-01-04 01:32:17 +0800

    npm build

--- public/build/assets/search-branch-section-7ba78dc5.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./app-7d68ca85.js";function n(){return r.jsx("div",{children:"SearchBranchSection"})}export{n as default};
---
+++ public/build/assets/search-branch-section-dd91e7a2.js
@@ -0,0 +1,1 @@
+import{j as r}from"./app-b503ba21.js";function n(){return r.jsx("div",{children:"SearchBranchSection"})}export{n as default};

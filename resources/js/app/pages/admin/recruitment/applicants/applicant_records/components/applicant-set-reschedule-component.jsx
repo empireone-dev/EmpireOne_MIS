0ec5422ec244f98380c16a5d2293c56d2aa71618
QuicklyDG <quicklydeguzman@gmail.com>
2025-07-31 12:22:46 +0800

    added reschedule

---
+++ resources/js/app/pages/admin/recruitment/applicants/applicant_records/components/applicant-set-reschedule-component.jsx
@@ -0,0 +1,25 @@
+import { Menu } from "antd";
+import React from "react";
+import { useState } from "react";
+import ApplicantChooseInterviewComponent from "./applicant-choose-interview-component";
+
+export default function ApplicantSetRescheduleComponent({ data, item, status }) {
+    const [open, setOpen] = useState(false);
+
+    function openHandler(params) {
+        setOpen(true);
+    }
+    return (
+        <>
+            <Menu.Item onClick={() => openHandler(true)} icon={item.icon}>
+                {item.label}
+            </Menu.Item>
+            <ApplicantChooseInterviewComponent
+                status={status}
+                setOpen={setOpen}
+                open={open}
+                data={data}
+            />
+        </>
+    );
+}

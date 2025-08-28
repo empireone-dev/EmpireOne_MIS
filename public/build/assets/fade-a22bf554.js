9d50699c3e3ec377e7425f1aa03818107a811861
QuicklyDG <quicklydeguzman@gmail.com>
2025-08-28 23:27:20 +0800

    added new adjustment

--- public/build/assets/fade-a22bf554.js
+++
@@ -1,4 +0,0 @@
-import"./genStyleUtils-5fa79f4a.js";import"./store-1e0cb636.js";import"./warning-58462ff8.js";import{K as e}from"./index-b5d03fc6.js";import{i as r}from"./useId-b72a7836.js";const m=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),s=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),d=function(i){let a=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=i,n=`${o}-fade`,t=a?"&":"";return[r(n,m,s,i.motionDurationMid,a),{[`
-        ${t}${n}-enter,
-        ${t}${n}-appear
-      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{d as i};

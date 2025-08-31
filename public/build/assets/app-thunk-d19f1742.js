d58f9022dff88104dbc29060be9740a065d6849b
QuicklyDG <quicklydeguzman@gmail.com>
2025-08-31 17:55:23 +0800

    adjust menu

---
+++ public/build/assets/app-thunk-d19f1742.js
@@ -0,0 +1,1 @@
+import{g as r}from"./user-service-9b4d0fce.js";import{a as s}from"./store-0f161645.js";function c(a){return async function(t,i){const e=await r();t(s.actions.setUser(e))}}export{c as g};

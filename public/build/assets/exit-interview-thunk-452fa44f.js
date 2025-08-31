470893ffe24c9146d6e69a11d5b85cb6c7836e2d
QuicklyDG <quicklydeguzman@gmail.com>
2025-08-31 14:01:26 +0800

    added adjusmtent

--- public/build/assets/exit-interview-thunk-452fa44f.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./exit-interview-service-34fc0962.js";import{e}from"./exit-interview-slice-5248b5db.js";function c(i){return async function(t,s){const n=await r(i);t(e.actions.setExitInterviews(n.data)),t(e.actions.setExitInterviewForm({}))}}export{c as s};
---
+++ public/build/assets/page-e69f2ea8.js
@@ -0,0 +1,1 @@
+import{j as r}from"./store-0f161645.js";import o from"./change-password-form-section-7d7c82c6.js";function t(){return r.jsx("div",{children:r.jsx(o,{})})}export{t as default};

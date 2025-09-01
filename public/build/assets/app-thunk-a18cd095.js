d0b373f6116208f2f74f8c8d25911a7342cbbb30
QuicklyDG <quicklydeguzman@gmail.com>
2025-09-01 18:43:50 +0800

    added date on signature

--- public/build/assets/app-thunk-a18cd095.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./user-service-bd80409f.js";import{a as s}from"./store-390fb298.js";function c(a){return async function(t,i){const e=await r();t(s.actions.setUser(e))}}export{c as g};
--- resources/js/app/pages/admin/file_201/view-document/page.jsx
+++ resources/js/app/pages/admin/file_201/view-document/page.jsx
@@ -3,7 +3,10 @@ import ViewDocumentSection from "./sections/view-document-section";
 import AdminLayout from "../../admin-layout";
 import { useEffect } from "react";
 import store from "@/app/store/store";
-import { get_onboarding_ackdoc_by_app_id_thunk } from "../redux/file-201-thunk";
+import {
+    get_e_signature_by_app_id_thunk,
+    get_onboarding_ackdoc_by_app_id_thunk,
+} from "../redux/file-201-thunk";
 import { get_onboarding_docu_by_id_thunk } from "../../onboarding/onboarding_docu/redux/onboarding-docu-thunk";
 import { get_applicant_by_app_id_thunk } from "../../final_rate/redux/final-rate-thunk";
 
@@ -14,6 +17,7 @@ export default function page() {
         store.dispatch(get_onboarding_ackdoc_by_app_id_thunk(app_id));
         store.dispatch(get_onboarding_docu_by_id_thunk(id));
         store.dispatch(get_applicant_by_app_id_thunk(app_id));
+        store.dispatch(get_e_signature_by_app_id_thunk(app_id));
     }, []);
     return (
         <>

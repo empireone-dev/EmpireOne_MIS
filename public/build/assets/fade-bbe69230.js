0aadeb54f9986b03b74ea2ca28489738aeec0141
QuicklyDG <quicklydeguzman@gmail.com>
2025-09-22 11:41:11 +0800

    adjust menu2

---
+++ public/build/assets/fade-bbe69230.js
@@ -0,0 +1,4 @@
+import"./genStyleUtils-83711dd6.js";import"./store-be97c391.js";import"./warning-ac26fe93.js";import{K as e}from"./index-005dae15.js";import{i as r}from"./useId-fe1cd0ca.js";const m=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),s=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),d=function(i){let a=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=i,n=`${o}-fade`,t=a?"&":"";return[r(n,m,s,i.motionDurationMid,a),{[`
+        ${t}${n}-enter,
+        ${t}${n}-appear
+      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{d as i};
--- resources/js/app/pages/admin/recruitment/applicants/applicant_records/sections/applicant-menu-section.jsx
+++ resources/js/app/pages/admin/recruitment/applicants/applicant_records/sections/applicant-menu-section.jsx
@@ -45,18 +45,18 @@ export default function ApplicantMenuSection({ data, interviewer }) {
                 />
             ),
         },
-        // {
-        //     component: (
-        //         <ApplicantDeleteComponent
-        //             item={{
-        //                 label: "Delete Application",
-        //                 key: "1",
-        //                 icon: <DeleteOutlined />,
-        //             }}
-        //             data={data}
-        //         />
-        //     ),
-        // },
+        {
+            component: (
+                <ApplicantDeleteComponent
+                    item={{
+                        label: "Delete Application",
+                        key: "1",
+                        icon: <DeleteOutlined />,
+                    }}
+                    data={data}
+                />
+            ),
+        },
         // {
         //     component: (
         //         <ApplicantCvFileComponent

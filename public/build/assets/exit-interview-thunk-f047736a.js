54ec36b5bd2805e2f8057feca83812aa4a2a7888
QuicklyDG <quicklydeguzman@gmail.com>
2025-09-24 15:13:39 +0800

    video

---
+++ public/build/assets/exit-interview-thunk-f047736a.js
@@ -0,0 +1,1 @@
+import{s as r}from"./exit-interview-service-7232ec82.js";import{m as e}from"./store-be97c391.js";function c(i){return async function(t,s){const n=await r(i);t(e.actions.setExitInterviews(n.data)),t(e.actions.setExitInterviewForm({}))}}export{c as s};

54d5617d11d7cad2f09daac4b9963cdeab666ecb
QuicklyDG <quicklydeguzman@gmail.com>
2025-10-23 09:32:32 +0800

    adjust app

--- public/build/assets/exit-interview-service-7232ec82.js
+++
@@ -1,1 +0,0 @@
-import{b as s}from"./store-be97c391.js";async function e(t){return(await s.post("/api/exit_int",t)).data}export{e as s};
---
+++ public/build/assets/page-3c896cb5.js
@@ -0,0 +1,1 @@
+import{j as r}from"./store-dfefbe8e.js";import o from"./change-password-form-section-89a3e26c.js";function t(){return r.jsx("div",{children:r.jsx(o,{})})}export{t as default};

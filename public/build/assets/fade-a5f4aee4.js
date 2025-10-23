150197ba56cef362b2e654cd25488b142ee6baf8
QuicklyDG <quicklydeguzman@gmail.com>
2025-10-23 09:33:54 +0800

    adjust application table

---
+++ public/build/assets/fade-a5f4aee4.js
@@ -0,0 +1,4 @@
+import"./genStyleUtils-83711dd6.js";import"./store-be97c391.js";import"./warning-ac26fe93.js";import{K as e}from"./index-e891d3f9.js";import{i as r}from"./useId-088a3768.js";const m=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),s=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),d=function(i){let a=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=i,n=`${o}-fade`,t=a?"&":"";return[r(n,m,s,i.motionDurationMid,a),{[`
+        ${t}${n}-enter,
+        ${t}${n}-appear
+      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{d as i};

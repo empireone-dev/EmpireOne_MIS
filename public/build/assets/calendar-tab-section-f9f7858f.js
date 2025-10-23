3bcbdc68f2d5e810c2cac244b08cb992838fd119
QuicklyDG <quicklydeguzman@gmail.com>
2025-10-23 09:35:47 +0800

    adjust application table

--- public/build/assets/calendar-tab-section-f9f7858f.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./store-be97c391.js";import o from"./calendar-component-ed97ea79.js";import"./ChevronDownIcon-59e5b216.js";import"./app-cc6dc99d.js";import"./portal-549fdb22.js";import"./use-server-handoff-complete-af2881a1.js";import"./floating-ui.dom-dbc86a7b.js";function s(){return r.jsx("div",{children:r.jsx(o,{})})}export{s as default};

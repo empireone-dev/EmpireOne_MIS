ad0e3d2ee805b93b2df528eb88cf6b6d6cc44960
QuicklyDG <quicklydeguzman@gmail.com>
2025-10-30 14:55:07 +0800

    router

--- resources/js/app/pages/admin/employee_relation/employee_section/components/file-201-component.jsx
+++ resources/js/app/pages/admin/employee_relation/employee_section/components/file-201-component.jsx
@@ -1,16 +1,18 @@
-import { Menu } from 'antd';
-import React, { useState } from 'react'
+import { router } from "@inertiajs/react";
+import { Menu } from "antd";
+import React, { useState } from "react";
 
 export default function File201Component({ data, item }) {
-  function openHandler(params) {
-    // setOpen(true);
-    window.open(`/admin/file_201/${data?.app_id}`, "_blank");
-  }
-  return (
-    <>
-      <Menu.Item onClick={() => openHandler(true)} icon={item.icon}>
-        {item.label}
-      </Menu.Item>
-    </>
-  )
+    function openHandler(params) {
+        // setOpen(true);
+        // window.open(`/admin/file_201/${data?.app_id}`, "_blank");
+        router.visit(`/admin/file_201/${data?.app_id}`);
+    }
+    return (
+        <>
+            <Menu.Item onClick={() => openHandler(true)} icon={item.icon}>
+                {item.label}
+            </Menu.Item>
+        </>
+    );
 }

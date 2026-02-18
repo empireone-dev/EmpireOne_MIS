cf7c842488559ad1d5c422dc40efc014007a90b6
QuicklyDG <quicklydeguzman@gmail.com>
2026-02-18 16:48:45 +0800

    npm build

---
+++ public/build/assets/fade-856c61a4.js
@@ -0,0 +1,4 @@
+import"./genStyleUtils-61b3e14c.js";import"./store-3c336f8b.js";import"./warning-61799e41.js";import{K as e}from"./index-92033e4d.js";import{i as r}from"./useId-e471c36f.js";const m=new e("antFadeIn",{"0%":{opacity:0},"100%":{opacity:1}}),s=new e("antFadeOut",{"0%":{opacity:1},"100%":{opacity:0}}),d=function(i){let a=arguments.length>1&&arguments[1]!==void 0?arguments[1]:!1;const{antCls:o}=i,n=`${o}-fade`,t=a?"&":"";return[r(n,m,s,i.motionDurationMid,a),{[`
+        ${t}${n}-enter,
+        ${t}${n}-appear
+      `]:{opacity:0,animationTimingFunction:"linear"},[`${t}${n}-leave`]:{animationTimingFunction:"linear"}}]};export{d as i};
